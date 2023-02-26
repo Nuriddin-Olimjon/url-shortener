4700bc994613d6c7a5ae1c560547f2af243d096a
AbdullohAbdullayev <abdullayev5109abdulloh@gmail.com>
2023-02-26 12:59:27 +0500

    ...

--- internal/domain/service/urls.go
+++ internal/domain/service/urls.go
@@ -1,1 +1,17 @@
 package service
+
+import (
+	"context"
+	"github.com/Nuriddin-Olimjon/url-shortener/internal/domain/entity"
+	"github.com/Nuriddin-Olimjon/url-shortener/internal/repository"
+)
+
+type UrlService interface {
+	CreateUrl(ctx context.Context, params entity.Url) (entity.Url, error)
+	GetUrlByID(ctx context.Context, id int32) (entity.Url, error)
+	GetUrlsByUserID(ctx context.Context, userID int32) ([]entity.Url, error)
+}
+
+type urlService struct {
+	repo repository.Store
+}
